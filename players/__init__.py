1030656035f56535ab6093dcc4f735dbcf16d812
Henry Dashwood <henry.dashwood@curationcorp.com>
2020-10-25 23:58:14 +0000

    and naughts and crosses code

--- players/__init__.py
+++
@@ -1,6 +0,0 @@
-from .dl import DeepLearning
-from .dl2 import DeepLearning2
-from .human import Human
-from .naive import Naive
-from .minimax import Minimax
-from .mcts import MCTS
--- players/naive.py
+++
@@ -1,8 +0,0 @@
-import chess
-import random
-import numpy as np
-
-class Naive:
-    def move(self, board):
-        move = random.choice(list(board.legal_moves))
-        return move.uci()
